da8550ae9b716630199c52fd04f763543cba8a02
dev.fringe@gmail.com <dev.fringe@gmail.com>
2016-06-10 16:22:20 +0900

    change mustache

--- src/main/java/dev/fringe/controller/RootController.java
+++ src/main/java/dev/fringe/controller/RootController.java
@@ -3,10 +3,7 @@ package dev.fringe.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 
 @Controller
