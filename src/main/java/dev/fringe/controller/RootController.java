f9d44c9dc38ba5ec17f3e3c42142dd10a9380e7c
dev.fringe@gmail.com <dev.fringe@gmail.com>
2016-06-10 14:48:26 +0900

    change mustache

--- src/main/java/dev/fringe/controller/RootController.java
+++ src/main/java/dev/fringe/controller/RootController.java
@@ -1,14 +1,24 @@
 package dev.fringe.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class RootController {
+
+    @Value("${application.message:Hello World}")
+    private String message = "Hello World";
+
     @RequestMapping("/")
-    @ResponseBody
-    String root() {
-        return "Hello World!";
+    String root(Map<String, Object> model) {
+        model.put("time", new Date());
+        model.put("message", this.message);
+        return "welcome";
     }
 }
