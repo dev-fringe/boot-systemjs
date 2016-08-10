d975ad7fbdb504f7bdf993aa95e4143b43dd6873
dev.fringe@gmail.com <dev.fringe@gmail.com>
2016-08-10 15:49:55 +0900

    add feature websocket

---
+++ src/main/java/dev/fringe/AtmosphereSupportConfigutration.java
@@ -0,0 +1,38 @@
+package dev.fringe;
+
+import org.atmosphere.cpr.AtmosphereServlet;
+import org.atmosphere.cpr.ContainerInitializer;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.util.Collections;
+
+@Configuration
+@EnableAutoConfiguration
+public abstract class AtmosphereSupportConfigutration {
+    @Bean
+    public EmbeddedAtmosphereInitializer atmosphereInitializer() {
+        return new EmbeddedAtmosphereInitializer();
+    }
+
+    @Bean
+    public ServletRegistrationBean atmosphereServlet() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new AtmosphereServlet(), "/message/*");
+        registration.addInitParameter("org.atmosphere.cpr.packages", "fringe");
+        registration.addInitParameter("org.atmosphere.interceptor.HeartbeatInterceptor"+ ".clientHeartbeatFrequencyInSeconds", "10");
+        registration.setLoadOnStartup(0);
+        registration.setOrder(Ordered.HIGHEST_PRECEDENCE);// Need to occur before the EmbeddedAtmosphereInitializer
+        return registration;
+    }
+
+    private static class EmbeddedAtmosphereInitializer extends ContainerInitializer implements ServletContextInitializer {
+        public void onStartup(ServletContext servletContext) throws ServletException {
+            onStartup(Collections.<Class<?>> emptySet(), servletContext);
+        }
+    }
+}
