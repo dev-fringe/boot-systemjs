572eae388577f7dc8eeb1560ca54b28bf55f4fe8
dev.fringe@gmail.com <dev.fringe@gmail.com>
2016-08-16 14:21:46 +0900

    change module archtype

---
+++ service/src/main/java/dev/fringe/FringeServiceApplication.java
@@ -0,0 +1,24 @@
+package dev.fringe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import java.util.Arrays;
+
+@SpringBootApplication
+@EnableSwagger2
+@ActiveProfiles("one")
+public class FringeServiceApplication {
+
+	public static void main(String[] args) throws Exception {
+		ApplicationContext ctx = SpringApplication.run(FringeServiceApplication.class, args);
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String beanName : beanNames) {
+			System.out.println(beanName);
+		}
+	}
+}
