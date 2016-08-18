1710473da80cece0a34da3646b51d95308802b63
dev.fringe@gmail.com <dev.fringe@gmail.com>
2016-08-18 14:40:17 +0900

    modify

--- core/src/main/java/dev/fringe/domain/Customer.java
+++ core/src/main/java/dev/fringe/domain/Customer.java
@@ -1,7 +1,9 @@
 package dev.fringe.domain;
 
+import lombok.Data;
 import javax.persistence.*;
 
+@Data
 @Entity
 @Table
 public class Customer {
@@ -18,64 +20,5 @@ public class Customer {
         this();
         this.firstName = firstName;
         this.lastName = lastName;
-    }
-
-    public long getId() {
-        return this.id;
-    }
-
-    public String getFirstName() {
-        return this.firstName;
-    }
-
-    public String getLastName() {
-        return this.lastName;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public boolean equals(Object o) {
-        if (o == this) return true;
-        if (!(o instanceof Customer)) return false;
-        final Customer other = (Customer) o;
-        if (!other.canEqual((Object) this)) return false;
-        if (this.getId() != other.getId()) return false;
-        final Object this$firstName = this.getFirstName();
-        final Object other$firstName = other.getFirstName();
-        if (this$firstName == null ? other$firstName != null : !this$firstName.equals(other$firstName)) return false;
-        final Object this$lastName = this.getLastName();
-        final Object other$lastName = other.getLastName();
-        if (this$lastName == null ? other$lastName != null : !this$lastName.equals(other$lastName)) return false;
-        return true;
-    }
-
-    public int hashCode() {
-        final int PRIME = 59;
-        int result = 1;
-        final long $id = this.getId();
-        result = result * PRIME + (int) ($id >>> 32 ^ $id);
-        final Object $firstName = this.getFirstName();
-        result = result * PRIME + ($firstName == null ? 43 : $firstName.hashCode());
-        final Object $lastName = this.getLastName();
-        result = result * PRIME + ($lastName == null ? 43 : $lastName.hashCode());
-        return result;
-    }
-
-    protected boolean canEqual(Object other) {
-        return other instanceof Customer;
-    }
-
-    public String toString() {
-        return "dev.fringe.domain.Customer(id=" + this.getId() + ", firstName=" + this.getFirstName() + ", lastName=" + this.getLastName() + ")";
     }
 }
