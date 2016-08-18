b51477ba33cc328eab7143b576a1e5f91169b793
dev.fringe@gmail.com <dev.fringe@gmail.com>
2016-08-18 14:03:41 +0900

    modify

--- persistence/src/main/java/dev/fringe/repository/CustomerRepository.java
+++ persistence/src/main/java/dev/fringe/repository/CustomerRepository.java
@@ -6,6 +6,5 @@ import dev.fringe.repository.custom.CustomerRepositoryCustom;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long>, CustomerRepositoryCustom {
-
     List<Customer> findByLastName(String lastName);
 }
